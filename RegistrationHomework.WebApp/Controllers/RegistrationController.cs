538617f1dfa9b552f52519cbb8ec457060805bd7
baroniunas91 <61449435+baroniunas91@users.noreply.github.com>
2020-12-22 17:46:40 +0200

    Add project files.

---
+++ RegistrationHomework.WebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationHomework.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationHomework.WebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post(RegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Index", model);
+            } else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ RegistrationHomework.WebApp/Models/RegistrationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationHomework.WebApp.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public string Age { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
